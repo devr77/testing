fde131ae95fb014c648acdf8e014c2cf02b35cb4
devr77 <draj56699@zoho.com>
2023-04-13 18:52:53 +0530

    lecture 2

--- main/src/App.js
+++ main/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import UserForm from "./UserForm";
 
 function App() {
   return (
     <div className="App">
+      <UserForm />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
